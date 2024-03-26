6e204cc94b0e88a5e8da7621c094ae978d3915c3
yash chokhani <yashchokhani95@gmail.com>
2024-03-26 14:43:19 +0530

    feat: aws clients added

---
+++ backend/src/aws/clients.js
@@ -0,0 +1,36 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  CognitoIdentityProviderClient,
+} = require("@aws-sdk/client-cognito-identity-provider");
+const { S3Client } = require("@aws-sdk/client-s3");
+const { SESClient } = require("@aws-sdk/client-ses");
+
+const dynamodbClient = new DynamoDBClient({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.MY_APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.MY_APP_AWS_SECRET_ACCESS_KEY,
+  },
+});
+const cognitoClient = new CognitoIdentityProviderClient({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.MY_APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.MY_APP_AWS_SECRET_ACCESS_KEY,
+  },
+});
+const s3Client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.MY_APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.MY_APP_AWS_SECRET_ACCESS_KEY,
+  },
+});
+const sesClient = new SESClient({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.MY_APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.MY_APP_AWS_SECRET_ACCESS_KEY,
+  },
+});
+module.exports = { dynamodbClient, cognitoClient, s3Client, sesClient };
