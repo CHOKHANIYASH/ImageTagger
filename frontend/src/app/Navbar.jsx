b5f2df2279f416adcd7bddfc5946dc21b3cd9a0e
yash chokhani <yashchokhani95@gmail.com>
2024-04-08 22:12:45 +0530

    feat: navbar updated

--- frontend/src/app/Navbar.jsx
+++ frontend/src/app/Navbar.jsx
@@ -1,105 +1,33 @@
+
 "use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "../components/ui/navbarMenu";
+import { cn } from "../utils/cn";
 
-import {
-  Box,
-  Flex,
-  Avatar,
-  HStack,
-  Text,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
-} from "@chakra-ui/react";
-// import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
-const Links = ["Dashboard", "Home", "History"];
-
-const NavLink = (props) => {
-  const { children } = props;
-
+export default function Navbar({ className }) {
+  const [active, setActive] = useState(null);
   return (
-    <Box
-      as="a"
-      px={2}
-      py={1}
-      rounded={"md"}
-      _hover={{
-        textDecoration: "none",
-        bg: useColorModeValue("gray.200", "gray.700"),
-      }}
-      href={"#"}
+    <div
+      className={cn("", className)}
     >
-      {children}
-    </Box>
-  );
-};
-
-export default function Navbar() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  return (
-    <>
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <IconButton
-            size={"md"}
-            // icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={"Open Menu"}
-            display={{ md: "none" }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={"center"}>
-            <HStack
-              as={"nav"}
-              spacing={4}
-              display={{ base: "none", md: "flex" }}
-            >
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </HStack>
-          </HStack>
-          <Flex alignItems={"center"}>
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={"full"}
-                variant={"link"}
-                cursor={"pointer"}
-                minW={0}
-              >
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                  }
-                />
-              </MenuButton>
-              <MenuList className="md:hidden">
-                {Links.map((link) => (
-                  <MenuItem key={link}>{link}</MenuItem>
-                ))}
-              </MenuList>
-            </Menu>
-          </Flex>
-        </Flex>
-
-        {isOpen ? (
-          <Box pb={4} display={{ md: "none" }}>
-            <Stack as={"nav"} spacing={4}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </Stack>
-          </Box>
-        ) : null}
-      </Box>
-    </>
+      <Menu setActive={setActive}>
+        <div className="md:hidden">
+        <MenuItem  setActive={setActive} active={active} item="menu" image="vercel.svg">
+          <div className="flex flex-col space-y-4 text-sm">
+          <HoveredLink href="/dashboard">Dashboard</HoveredLink>
+            <HoveredLink href="/">Home</HoveredLink>
+            <HoveredLink href="/history">History</HoveredLink>
+            <HoveredLink href="/login">Login</HoveredLink>
+            <HoveredLink href="/signup">Signup</HoveredLink>
+          </div>
+        </MenuItem>
+        </div>
+            <HoveredLink className='max-md:hidden' href="/dashboard">Dashboard</HoveredLink>
+            <HoveredLink className='max-md:hidden' href="/">Home</HoveredLink>
+            <HoveredLink className='max-md:hidden' href="/history">History</HoveredLink>
+            <HoveredLink className='max-md:hidden' href="/login">Login</HoveredLink>
+            <HoveredLink className='max-md:hidden' href="/signup">Signup</HoveredLink>
+      </Menu>
+    </div>
   );
 }
---
+++ frontend/src/components/ui/navbarMenu.jsx
@@ -0,0 +1,84 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
+
+const transition = {
+  type: "spring",
+  mass: 0.5,
+  damping: 11.5,
+  stiffness: 100,
+  restDelta: 0.001,
+  restSpeed: 0.001,
+};
+
+export const MenuItem = ({
+  setActive,
+  active,
+  item,
+  image,
+  children,
+}) => {
+  return (
+    <div onMouseEnter={() => setActive(item)} className="relative ">
+      <motion.p
+        transition={{ duration: 0.3 }}
+        className="cursor-pointer text-black hover:opacity-[0.9]"
+      >
+        <Image src={image} width={50} height={50} alt={item} />
+      </motion.p>
+      {active !== null && (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.85, y: 10 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={transition}
+        >
+          {active === item && (
+            <div className="absolute top-[calc(100%_+_1.7rem)] left-1/2 transform -translate-x-1/2">
+              <motion.div
+                transition={transition}
+                layoutId="active" // layoutId ensures smooth animation
+                className="bg-white backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2]shadow-xl"
+              >
+                <motion.div
+                  layout // layout ensures smooth animation
+                  className="h-full p-4 w-max"
+                >
+                  {children}
+                </motion.div>
+              </motion.div>
+            </div>
+          )}
+        </motion.div>
+      )}
+    </div>
+  );
+};
+
+export const Menu = ({
+  setActive,
+  children,
+}) => {
+  return (
+    <nav
+      onMouseLeave={() => setActive(null)} // resets the state
+      className="relative flex justify-center px-8 py-6 space-x-4 bg-white border-transparent max-md:justify-end boder shadow-input "
+    >
+      {children}
+    </nav>
+  );
+};
+
+
+
+export const HoveredLink = ({ children,className, href }) => {
+  return (
+    <Link
+      href={href}
+      className={`${className}  text-neutral-700 hover:text-black `}
+    >
+      {children}
+    </Link>
+  );
+};
