b9a49c99490d6caec6109f1ecfbcf3606504f2a3
yash chokhani <yashchokhani95@gmail.com>
2024-04-09 18:34:46 +0530

    feat: tag page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,6 @@
+// const {
+//   default: flattenColorPalette,
+// } = require("tailwindcss/lib/util/flattenColorPalette");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,6 +10,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -16,3 +22,12 @@ module.exports = {
   },
   plugins: [],
 };
+// function addVariablesForColors({ addBase, theme }) {
+//   let allColors = flattenColorPalette(theme("colors"));
+//   let newVars = Object.fromEntries(
+//     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+//   );
+//   addBase({
+//     ":root": newVars,
+//   });
+// }
