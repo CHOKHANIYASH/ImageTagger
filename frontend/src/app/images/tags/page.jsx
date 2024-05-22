72c3d0314961d49cd8e33f9b520daca6f52037e9
yash chokhani <yashchokhani95@gmail.com>
2024-05-22 20:14:17 +0530

    feat: access_token added to axios in tags page

--- frontend/src/app/images/tags/page.jsx
+++ frontend/src/app/images/tags/page.jsx
@@ -5,15 +5,21 @@ import { useState, useEffect } from "react";
 import { CardBody, CardContainer, CardItem } from "../../../components/ui/card";
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
-
+import { useAppSelector } from "@/redux/hooks/index";
+import { headers } from "next/headers";
 export default function Tag() {
   const searchParams = useSearchParams();
   const imageUrl = searchParams.get("imageUrl");
   const image = imageUrl.split("/").pop();
   const [tags, setTags] = useState([]);
+  const accessToken = useAppSelector((state) => state.auth.accessToken);
   useEffect(() => {
     axios
-      .get(`${process.env.NEXT_PUBLIC_SERVER_DEV_URL}/images/labels/${image}`)
+      .get(`${process.env.NEXT_PUBLIC_SERVER_DEV_URL}/images/labels/${image}`, {
+        headers: {
+          access_token: `${accessToken}`,
+        },
+      })
       .then((response) => {
         setTags(response.data.data);
       });
